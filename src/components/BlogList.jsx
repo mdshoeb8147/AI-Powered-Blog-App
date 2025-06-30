ea4b6b382a2d7839e8bcf186949dfbbd8122a153
mdshoeb8147 <mdshoeb8147@gmail.com>
2025-06-30 23:22:40 +0530

    error in bloglist

---
+++ src/components/BlogList.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import { blogCategories } from "../assets/assets";
+
+const BlogList = () => {
+  const [menu, setMenu] = useState("All");
+  return (
+    <div>
+      <div className="flex justify-center gap-4 sm:gap-8 my-10 relative">
+        {blogCategories.map((item) => (
+          <div className="relative" key={item}>
+            <button
+              onClick={() => setMenu(item)}
+              className={`cursor-pointer text-gray-500 ${
+                menu === item && "text-white px-4 p-0.5"
+              }`}
+            >
+              {item}
+              <div className="absolute left-0 right-0 top-0 h-7 -z-1 bg-blue-500 rounded-full"></div>
+            </button>
+          </div>
+        ))}
+      </div>
+
+      <div className="">{/* blog cards */}</div>
+    </div>
+  );
+};
+export default BlogList;
