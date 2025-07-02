433e55891d95724eff9e172ec505d124a48a17ed
mdshoeb8147 <mdshoeb8147@gmail.com>
2025-07-02 23:54:53 +0530

    completed with home page with footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,39 @@
+import { assets, footer_data } from "../assets/assets";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="px-6 md:px-16 lg:px-24 xl:px-32 bg-blue-500/3">
+      <div className="flex flex-col md:flex-row items-start justify-between gap-10 py-10 border-b border-gray-500/30 text-gray-500">
+        <div className="w-32 sm:w-44">
+          <img src={assets.logo} alt="logo" />
+          <p className="max-w-[410px] mt-6">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Recusandae, earum sapiente.
+          </p>
+        </div>
+        <div className="flex flex-wrap justify-between w-full md:w-[45%] gap-5">
+          {footer_data.map((section, index) => (
+            <div className="" key={index}>
+              <h3 className="font-semibold text-base text-gray-900 md:mb-5 mb-2">
+                {section.title}
+              </h3>
+              <ul className="text-sm space-y-1">
+                {section.links.map((link, i) => (
+                  <li key={i}>
+                    <a href="#" className="hover:underline transition">
+                      {link}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      </div>
+      <p className="py-4 text-center text-sm md:text-base text-gray-500">
+        Copyright 2025 @ QuickBlog - All Right Reserved{" "}
+      </p>
+    </div>
+  );
 };
 export default Footer;
